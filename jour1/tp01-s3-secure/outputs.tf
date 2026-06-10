806ad49ee451e23b09ee39c3ffc706104bbe1444
Dna-0324 <bdkt237@gmail.com>
2026-06-10 09:53:21 +0200

    fix: tp01-s3-secure en dossier normal (plus de submodule)

---
+++ jour1/tp01-s3-secure/outputs.tf
@@ -0,0 +1,24 @@
+output "bucket_name" {
+  value       = aws_s3_bucket.main.id
+  description = "Nom du bucket S3"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.main.arn
+  description = "ARN complet du bucket"
+}
+
+output "bucket_region" {
+  value       = aws_s3_bucket.main.region
+  description = "Region AWS du bucket"
+}
+
+output "versioning_status" {
+  value       = aws_s3_bucket_versioning.main.versioning_configuration[0].status
+  description = "Statut du versioning"
+}
+
+output "bucket_region_name" {
+  value       = data.aws_region.current.name
+  description = "Region AWS du bucket (via data source)"
+}
